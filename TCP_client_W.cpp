fdd303bd31dfb236cb5fe09ed69cd5ec5dfbfc65
R0gal1k <95590314+R0gfall@users.noreply.github.com>
2024-08-01 00:03:41 +0300

    Добавьте файлы проекта.

---
+++ TCP_client_W.cpp
@@ -0,0 +1,58 @@
+﻿#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#include <stdio.h>
+#include <string.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+
+int init()
+{
+	WSADATA wsa_data;
+	return (WSAStartup(MAKEWORD(2, 2), &wsa_data));
+}
+
+
+void deinit()
+{
+	WSACleanup();
+}
+
+
+ADDRINFO install_properties(void)
+{
+	ADDRINFO hints;
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+	return hints;
+}
+
+
+
+int main()
+{
+	int result_conn, result_info;
+	SOCKET ConnectSocket = INVALID_SOCKET;
+	ADDRINFO* addr_result = NULL;
+	
+	ADDRINFO hints = install_properties();
+
+	result_info = getaddrinfo("localhost", "9000", &hints, &addr_result);
+
+	if (result_info != 0) {
+		printf("ERROR GET ADDRESS INFO");
+		return -1;
+	}
+
+
+	ConnectSocket = socket(addr_result->ai_family, addr_result->ai_socktype, addr_result->ai_protocol);
+
+
+}
